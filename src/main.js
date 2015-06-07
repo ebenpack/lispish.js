ccbd3de8265a549a8ac2341d906f3575bfcb17a5
ebenpack <ebenpackwood@gmail.com>
2015-06-07 18:50:11 -0400

    Update main

--- src/main.js
+++ src/main.js
@@ -1,25 +1,19 @@
-var cons = require('./cons/cons.js');
-var car = require('./cons/car.js');
-var cdr = require('./cons/cdr.js');
-var print = require('./cons/print.js');
+// cons
+exports.cons = require('./cons/cons.js');
+exports.car = require('./cons/car.js');
+exports.cdr = require('./cons/cdr.js');
+exports.print = require('./cons/print.js');
+exports.equal = require('./cons/equal.js');
+exports.pair = require('./cons/pair.js');
 
-var list = require('./list/list.js');
-var map = require('./list/map.js');
-var reduce = require('./list/reduce.js');
-var filter = require('./list/filter.js');
-var peek = require('./list/peek.js');
-var push = require('./list/push.js');
-var pop = require('./list/pop.js');
-
-exports.cons = cons;
-exports.car = car;
-exports.cdr = cdr;
-exports.print = print;
-
-exports.list = list;
-exports.map = map;
-exports.reduce = reduce;
-exports.filter = filter;
-exports.peek = peek;
-exports.push = push;
-exports.pop = pop;
+// list
+exports.list = require('./list/list.js');
+exports.map = require('./list/map.js');
+exports.reduce = require('./list/reduce.js');
+exports.filter = require('./list/filter.js');
+exports.peek = require('./list/peek.js');
+exports.push = require('./list/push.js');
+exports.pop = require('./list/pop.js');
+exports.zip = require('./list/zip.js');
+exports.enqueue = require('./list/enqueue.js');
+exports.dequeue = require('./list/dequeue.js');
