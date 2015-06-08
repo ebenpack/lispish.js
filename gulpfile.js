85a91b1b6cabbf6955920d3674d45946ce412d47
ebenpack <ebenpackwood@gmail.com>
2015-06-07 23:06:19 -0400

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,22 +12,17 @@ gulp.task('browserify', function() {
         .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('browserify-test', function() {
-    return brwsrfy('./tests/main.js', {standalone: 'test'})
-        .bundle()
-        //Pass desired output filename to vinyl-source-stream
-        .pipe(source('test.js'))
-        // Start piping stream to tasks!
-        .pipe(gulp.dest('./tests/'));
-});
-
 gulp.task('test', function () {
     return gulp.src('./tests/tests.js', {read: false})
         .pipe(mocha({reporter: 'min', ui: 'tdd'}));
 });
 
-gulp.task('testwatch', function() {
+gulp.task('test-watch', function() {
   gulp.watch(['./src/**/*.js', './tests/**/*.js'], ['test']);
 });
 
-gulp.task('default', ['testwatch']);
+gulp.task('browserify-watch', function() {
+  gulp.watch(['./src/**/*.js'], ['browserify']);
+});
+
+gulp.task('default', ['browserify-watch']);
