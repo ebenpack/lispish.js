3432b7268d8d2b8a33fc7036566e68c5cc262715
ebenpack <ebenpackwood@gmail.com>
2015-06-14 08:38:37 -0400

    Update main

--- src/main.js
+++ src/main.js
@@ -26,6 +26,7 @@ exports.push = require('./list/push.js');
 exports.pop = require('./list/pop.js');
 exports.zip = require('./list/zip.js');
 exports.reverse = require('./list/reverse.js');
+exports.concat = require('./list/concat.js');
 exports.enqueue = require('./list/enqueue.js');
 exports.dequeue = require('./list/dequeue.js');
 exports.slice = require('./list/slice.js');
